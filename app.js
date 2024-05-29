ce407551b0ce04fa46242a18a583394b22e26bef
Pavan Kumar <pkumar.rathor@nic.in>
2024-05-29 18:00:10 +0530

    update auth middleware

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const authRoute = require("./routes/authPage");
 const ipRoute = require("./routes/ips")
 const webRoute = require("./routes/web")
 const express = require("express");
-
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', webRoute);
 app.use('/api', userRoute);
--- routes/authPage.js
+++ routes/authPage.js
@@ -4,6 +4,10 @@ const { dashboard, profile } = require('../controller/authPage');
 
 const router = express.Router();
 
+router.get('/validate-token', auth, (req, res) => {
+    res.status(200).json({ 'status': 'success', 'message': 'Token is valid' });
+});
+
 router.post('/dashboard', auth, dashboard);
 
 router.post('/profile', auth, profile);
